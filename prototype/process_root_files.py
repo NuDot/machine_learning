cff67a9d094d64d67d043ae7fd7c0a77a22e73f8
aobol <liaobo77@gmail.com>
2018-03-21 12:23:11 -0400

    camera rotator

--- prototype/process_root_files.py
+++ prototype/process_root_files.py
@@ -57,6 +57,8 @@ def random_decision(pressure):
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", default="sph_out_topology180_center_NoMultScat_100.root")
 parser.add_argument("--type", "-t", help="Type of MC files, 1 ring or 2 ring", default = 1)
+parser.add_argument("--theta","-th", help="Rotate Camera with given theta(0 - 2pi)",type = float, default = 0)
+parser.add_argument("--phi","-ph", help="Rotate Camera with given phi(0 - pi)",type = float, default = 0)
 args = parser.parse_args()
 
 #Global Variables
@@ -67,6 +69,8 @@ max_pressure=10
 n_pmts=5000
 cols = int(math.sqrt(n_pmts/2))
 rows = cols *2
+row_rotation = int(math.fmod(args.theta, (2 * math.pi)) / (2 * math.pi) * rows)
+col_rotation = int(math.fmod(args.phi, math.pi) / math.pi * cols)
 
 first_dimension = int((final_time - initial_time) / time_interval) + 1
 second_dimension = max_pressure + 1
@@ -93,8 +97,17 @@ for event in tree:
           if (tree.PE_creation[i]) or random_decision(max_pressure-pressure_pe):
             feature_map_collections[time_index][pressure_pe][row][col] += 100
 
+  for index_f, first_layer in enumerate(feature_map_collections):
+    for index_s, target_map in enumerate(first_layer):
+      if not ((row_rotation == 0) or (row_rotation == 1)):
+        top, bottom = np.split(target_map, [row_rotation], axis=0)
+        target_map = np.concatenate((bottom, top), axis=0)
+      if not ((col_rotation == 0) or (col_rotation == 1)):
+        left, right = np.split(target_map, [col_rotation], axis=1)
+        target_map = np.concatenate((right, left), axis = 1)
+      feature_map_collections[index_f][index_s] = target_map
 
 
-plt.imshow(feature_map_collections[3][10])
+plt.imshow(feature_map_collections[2][0])
 plt.show()
 
